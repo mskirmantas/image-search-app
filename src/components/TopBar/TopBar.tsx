15245b2844ef28f3676279278aebdc617b86bbdf
mantasskirma <56951458+mantasskirma@users.noreply.github.com>
2020-03-06 16:34:47 +0200

    Added search button + styling

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import axios from "axios";
 import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
 
 export const TopBar: React.FC = () => {
   const [search, setSearch] = React.useState("");
@@ -12,25 +14,40 @@ export const TopBar: React.FC = () => {
     setSearch("");
   };
 
+  const handleSubmit = () => {};
+
   return (
     <div className="TopBar">
       <div className="logo">
         <h1>Unsearch</h1>
       </div>
-
-      <div className="search-field">
-        <Icon className="search-bar-icon">search</Icon>
-        <input
-          type="text"
-          placeholder="Search free high-resolution images"
-          value={search}
-          onChange={updateSearch}
-        />
-        {search ? (
-          <Icon className="search-bar-icon clear-icon" onClick={clearSearch}>
-            close
-          </Icon>
-        ) : null}
+      <div className="search-bar">
+        <div className="search-field">
+          <Icon className="search-field-icon">search</Icon>
+          <input
+            type="text"
+            placeholder="Search free high-resolution images"
+            value={search}
+            onChange={updateSearch}
+          />
+          {search ? (
+            <Icon
+              className="search-field-icon clear-icon"
+              onClick={clearSearch}
+            >
+              close
+            </Icon>
+          ) : null}
+        </div>
+        <Button
+          style={{ margin: "auto 5px", backgroundColor: "#1db954" }}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Search
+        </Button>
       </div>
     </div>
   );
