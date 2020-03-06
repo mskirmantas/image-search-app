1911f172e7b4a59742460f5641a117e0cecaeab0
mantasskirma <56951458+mantasskirma@users.noreply.github.com>
2020-03-06 12:48:10 +0200

    Added search-field + basic styling

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Icon from "@material-ui/core/Icon";
 
 export const TopBar: React.FC = () => {
-  return <div className="TopBar"></div>;
+  return (
+    <div className="TopBar">
+      <div className="search-field">
+        <Icon className="search-bar-icon">search</Icon>
+        <input type="text" placeholder="Search free high-resolution images" />
+
+        <Icon className="search-bar-icon clear-icon">close</Icon>
+      </div>
+    </div>
+  );
 };
