822d2e875a37d33b26daa226a599bd4e9705b593
mantasskirma <56951458+mantasskirma@users.noreply.github.com>
2020-03-06 13:25:13 +0200

    Added search-field actions

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -2,16 +2,35 @@ import React from "react";
 import Icon from "@material-ui/core/Icon";
 
 export const TopBar: React.FC = () => {
+  const [search, setSearch] = React.useState("");
+
+  const updateSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value.substring(0, 50));
+  };
+
+  const clearSearch = () => {
+    setSearch("");
+  };
+
   return (
     <div className="TopBar">
       <div className="logo">
         <h1>Unsearch</h1>
       </div>
+
       <div className="search-field">
         <Icon className="search-bar-icon">search</Icon>
-        <input type="text" placeholder="Search free high-resolution images" />
-
-        <Icon className="search-bar-icon clear-icon">close</Icon>
+        <input
+          type="text"
+          placeholder="Search free high-resolution images"
+          value={search}
+          onChange={updateSearch}
+        />
+        {search ? (
+          <Icon className="search-bar-icon clear-icon" onClick={clearSearch}>
+            close
+          </Icon>
+        ) : null}
       </div>
     </div>
   );
